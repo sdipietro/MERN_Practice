99122e5582fb5f99d2c2c3b5315c57e0fd9c75a8
Stephen DiPietro <stevedipietro97@gmail.com>
2020-02-24 03:53:24 -0500

    Finish routes and validation

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const db = require("./config/keys").mongoURI;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("MERN Practice"));
+
+app.get("/", (req, res) => {
+    res.send("MERN Practice");
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
